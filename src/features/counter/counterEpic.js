673fb5c0d8e5fe4a1c74368e9668310764c8dd8b
Bo Chen <bochen2014@yahoo.com>
2021-02-19 23:54:26 +1100

    update

--- src/features/counter/counterEpic.js
+++ src/features/counter/counterEpic.js
@@ -1,10 +1,11 @@
 import { ofType } from "redux-observable";
-import { map, delay } from "rxjs/operators";
+import { map, delay, filter } from "rxjs/operators";
 import { incrementAsync, incrementByAmount } from "./counterSlice";
 
 const counterEpic = (action$) =>
   action$.pipe(
-    ofType(incrementAsync.type),
+    //  ofType(incrementAsync.type), // this works, verified;
+    filter(incrementAsync.match),
     delay(1000),
     map(({ payload }) => incrementByAmount(payload))
   );
